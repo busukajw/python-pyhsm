7617b6bd2d8fe26ef077cc00f752ff52a44d9adc
Aaron Walker <aaron.walker@booking.com>
2014-11-15 10:34:22 +0100

    Created directory structure to help maintain app as it gets a little larger

---
+++ yhsm-val/config/development.py
@@ -0,0 +1,24 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+
+
+SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost:3306/val'
+HSM = 'yhsm://localhost:5348'
+KEY_HANDLE = '1'
+
+# Specify how often the sync daemon awakens
+__YKVAL_SYNC_INTERVAL__ = 10;
+# Specify how long the sync daemon will wait for response
+__YKVAL_SYNC_RESYNC_TIMEOUT__ = 30;
+# Specify how old entries in the database should be considered aborted attempts
+__YKVAL_SYNC_OLD_LIMIT__ = 10;
+
+# These are settings for the validation server.
+__YKVAL_SYNC_FAST_LEVEL__ = 1;
+__YKVAL_SYNC_SECURE_LEVEL__ = 40;
+__YKVAL_SYNC_DEFAULT_LEVEL__ = 60;
+__YKVAL_SYNC_DEFAULT_TIMEOUT__ = 1;
+__YKVAL_SYNC_POOL__ = ['localhost', '192.192.1.1']
+
---
+++ yhsm-val/run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import os
+from app import create_app, db
+
+if __name__ == '__main__':
+    app = create_app(os.environ.get('FLASK_CONFIG', 'development'))
+    with app.app_context():
+        db.create_all()
+    app.run()
